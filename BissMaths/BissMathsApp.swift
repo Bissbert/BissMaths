8c4072464ec7278bb2a041e3e0d569a29f0fbc30
Bissbert <43237892+Bissbert@users.noreply.github.com>
2020-09-27 12:14:05 +0200

    Initial Commit

---
+++ BissMaths/BissMathsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BissMathsApp.swift
+//  BissMaths
+//
+//  Created by Fabian Moor on 27.09.20.
+//
+
+import SwiftUI
+
+@main
+struct BissMathsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
